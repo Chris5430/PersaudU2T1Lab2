89c516f0b332917ca3401e0273b7c43fbd823032
Christopher Persaud <christopherp543@nycstudents.net>
2022-09-30 11:22:26 -0400

    ran circle

---
+++ src/CircleRunner.java
@@ -0,0 +1,16 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle jeff = new Circle(5);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        jeff.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        //I think it will print "My area is 78.5"
+
+        // D. run the code and confirm your prediction! Were you correct?
+        // I was correct.
+    }
+}
